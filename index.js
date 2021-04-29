fd33b0180d1888a43dbbce082e0fbe834c7ea0b4
jatin-verma-1999 <jatin71_soe@jnu.ac.in>
2021-04-29 19:41:36 +0530

    mobile modal done

--- index.js
+++ index.js
@@ -139,13 +139,14 @@ secondButton.addEventListener("click",function(){
 
       
 })
-
+console.log($(window).width());
 
  // Get the modal
  var modal = document.getElementById("myModal1");
-
+ var modal2=document.getElementById("myModal2");
  // Get the button that opens the modal
  var btn = document.getElementById("myBtn1");
+ var btn2=document.getElementsByClassName("myBtn2")[0];
 
  // Get the <span> element that closes the modal
  var span = document.getElementsByClassName("close1")[0];
@@ -154,15 +155,28 @@ secondButton.addEventListener("click",function(){
  btn.onclick = function () {
    modal.style.display = "block";
  }
-
+ 
+ if($(window).width() <=768)
+ {
+  btn2.onclick = function () {
+    modal2.style.display = "block";
+  }
+ }
+ 
  // When the user clicks on <span> (x), close the modal
  span.onclick = function () {
    modal.style.display = "none";
  }
+
+ 
 
  // When the user clicks anywhere outside of the modal, close it
  window.onclick = function (event) {
    if (event.target == modal) {
      modal.style.display = "none";
    }
+   else if(event.target==modal2)
+   modal2.style.display="none";
  }
+
+ 
